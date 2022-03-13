70ea603758b1b3eee7c3c6ecd2394556d9fca54e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-13 17:28:17 -0400

    Intitial Ember.

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'antirubbersheeter/app';
+import config from 'antirubbersheeter/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
