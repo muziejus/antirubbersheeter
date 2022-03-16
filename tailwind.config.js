06e8340a47b6e9e8943e6f999eadea1dacf578a9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-16 00:15:51 -0400

    feat: Get some sensible styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ const relevantFilesGlob = "**/*.{html,js,ts,hbs,gjs,gts}";
 module.exports = {
   content: [path.join(appEntry, relevantFilesGlob)],
   theme: {
+    colors: {
+      white: "#f9f9f9",
+      "light-gray": "#efefef",
+      tan: "#d2d2c0",
+      gray: "#555555",
+      blue: "#41516c",
+      "dark-blue": "#093552",
+    },
     extend: {},
     fontFamily: {
       title: ["Herr Von Muellerhoff", "cursive"],
+      serif: ["Hina Mincho", "ui-serif", "Times New Roman", "serif"],
     },
   },
   plugins: [],
