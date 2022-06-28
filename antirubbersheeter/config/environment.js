82f66fda4e76637e0b21e4e7460ef52add10dc0c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-06-27 20:21:57 -0400

    feat: change rooturl in production.

--- antirubbersheeter/config/environment.js
+++ antirubbersheeter/config/environment.js
@@ -45,6 +45,7 @@ module.exports = function (environment) {
 
   if (environment === "production") {
     // here you can enable a production-specific feature
+    ENV.rootURL = "/antirubbersheeter-ember-application/";
   }
 
   return ENV;
