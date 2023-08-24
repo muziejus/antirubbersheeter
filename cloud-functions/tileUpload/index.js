93a0588c39113c383997ce4984e0e185105e62ac
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-08-24 15:34:39 -0400

    feat: Begin tileUpload function.

---
+++ cloud-functions/tileUpload/index.js
@@ -0,0 +1,16 @@
+const functions = require("@google-cloud/functions-framework");
+
+// Register a CloudEvent callback with the Functions Framework that will
+// be triggered by Cloud Storage.
+functions.cloudEvent("tileUpload", (cloudEvent) => {
+  console.log(`Event ID: ${cloudEvent.id}`);
+  console.log(`Event Type: ${cloudEvent.type}`);
+
+  const file = cloudEvent.data;
+  console.log(`Bucket: ${file.bucket}`);
+  console.log(`File: ${file.name}`);
+  console.log(`Metageneration: ${file.metageneration}`);
+  console.log(`Created: ${file.timeCreated}`);
+  console.log(`Updated: ${file.updated}`);
+});
+// [END functions_cloudevent_storage]
